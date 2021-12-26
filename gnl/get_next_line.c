3c45b2fa21801c654b0334132879f6c76d321df1
Toysin <toysin1203@hotmail.com>
2021-12-27 02:03:11 +0900

    Upload gnl

---
+++ gnl/get_next_line.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donshin <donshin@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/24 13:27:59 by donshin           #+#    #+#             */
+/*   Updated: 2021/12/26 18:37:13 by donshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_string_adder(int fd, char *save);
+static char	*ft_get_line(char *save);
+static char	*ft_save_remain(char *save);
+
+char	*get_next_line(int fd)
+{
+	static char	*save;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	save = ft_string_adder(fd, save);
+	if (!save)
+		return (NULL);
+	line = ft_get_line(save);
+	save = ft_save_remain(save);
+	return (line);
+}
+
+static char	*ft_string_adder(int fd, char *save)
+{
+	ssize_t	rbyte;
+	char	*buf;
+
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	rbyte = fd + 1;
+	while (rbyte > 0 && !ft_my_strchr(save, '\n'))
+	{
+		rbyte = read(fd, buf, BUFFER_SIZE);
+		if (rbyte == -1)
+			break ;
+		buf[rbyte] = '\0';
+		save = ft_my_strjoin(save, buf);
+	}
+	free(buf);
+	if (rbyte == -1)
+		return (NULL);
+	return (save);
+}
+
+static char	*ft_get_line(char *save)
+{
+	char	*line;
+	char	*nl_ptr;
+
+	if (*save == '\0')
+		return (NULL);
+	nl_ptr = ft_my_strchr(save, '\n');
+	if (!nl_ptr)
+		return (ft_strdup(save));
+	line = (char *)malloc(sizeof(char) * (nl_ptr - save + 2));
+	if (!line)
+		return (NULL);
+	ft_my_strlcpy(line, save, nl_ptr - save + 2);
+	return (line);
+}
+
+static char	*ft_save_remain(char *save)
+{
+	char	*line;
+	char	*nl_ptr;
+
+	nl_ptr = ft_my_strchr(save, '\n');
+	if (!nl_ptr)
+	{
+		free(save);
+		return (NULL);
+	}
+	line = ft_strdup(nl_ptr + 1);
+	free(save);
+	return (line);
+}
---
+++ gnl/get_next_line_utils_bonus.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donshin <donshin@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/24 13:40:28 by donshin           #+#    #+#             */
+/*   Updated: 2021/12/27 01:33:56 by donshin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	ft_my_strlcpy(char *dest, const char *src, size_t dstsize)
+{
+	size_t	index;
+
+	index = 0;
+	while (index + 1 < dstsize && src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	if (dstsize)
+		dest[index] = '\0';
+}
+
+char	*ft_my_strchr(char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
+
+char	*ft_my_strjoin(char *s1, char *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*comb;
+
+	if (!s1)
+		return (ft_strdup(s2));
+	s1_len = 0;
+	while (s1[s1_len])
+		s1_len++;
+	s2_len = 0;
+	while (s2[s2_len])
+		s2_len++;
+	comb = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!comb)
+		return (NULL);
+	ft_my_strlcpy(comb, s1, s1_len + 1);
+	ft_my_strlcpy(comb + s1_len, s2, s2_len + 1);
+	free(s1);
+	return (comb);
+}
+
+char	*ft_strdup(char *s)
+{
+	size_t	len;
+	char	*dup;
+
+	if (!s)
+		return (NULL);
+	len = 0;
+	while (s[len])
+		len++;
+	dup = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dup)
+		return (NULL);
+	ft_my_strlcpy(dup, s, len + 1);
+	return (dup);
+}
+
+void	ft_my_lstdelone(t_save *lst)
+{
+	lst->prev->next = lst->next;
+	if (lst->next)
+		lst->next->prev = lst->prev;
+	free(lst);
+}
