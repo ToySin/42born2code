a1f4eedb89483e7baa7cf67454d5396728816724
Toysin <toysin1203@hotmail.com>
2022-01-02 23:48:52 +0900

    Create converting functions

--- ft_printf/convert_arg.c
+++ ft_printf/convert_arg.c
@@ -6,7 +6,7 @@
 /*   By: donshin <donshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 23:28:12 by donshin           #+#    #+#             */
-/*   Updated: 2022/01/02 23:28:48 by donshin          ###   ########.fr       */
+/*   Updated: 2022/01/02 23:44:29 by donshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,33 @@
 
 void	convert_arg(t_info *info, va_list ap)
 {
+	char type;
 
+	type = info->type;
+
+	if (ft_strcmp("c", type))
+		convert_char(info, va_arg(ap, int));
+	else if (ft_strcmp("s", type))
+		convert_stype(info, va_arg(ap, char *));
+	else if (ft_strcmp("p", type))
+	{
+		convert_
+	}
+	else if (ft_strchr("di", type))
+	{
+
+	}
+	else if (ft_strcmp("u", type))
+	{
+
+	}
+	else if (ft_strchr("xX", type))
+	{
+
+	}
+	else if (ft_strcmp("%", type))
+	{
+		info->converted_str = ft_strdup("%");
+		info->arg_len = 1;
+	}
 }
