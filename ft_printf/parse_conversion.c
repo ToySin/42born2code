1299341e0e2ed07db968116b75ac10dc12391860
Toysin <toysin1203@hotmail.com>
2022-01-03 23:22:17 +0900

    format ptr adding operator

--- ft_printf/parse_conversion.c
+++ ft_printf/parse_conversion.c
@@ -6,7 +6,7 @@
 /*   By: donshin <donshin@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 15:32:01 by donshin           #+#    #+#             */
-/*   Updated: 2022/01/03 16:51:14 by donshin          ###   ########.fr       */
+/*   Updated: 2022/01/03 17:55:08 by donshin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 void	parse_conversion(const char **format, va_list ap, int *byte)
 {
-	const char	*format_ptr;
-
-	format_ptr = *(++format);
-	write(1, format_ptr, 1); //이것도 안나오네..
-	print_arg(*format_ptr++, ap, byte);
-	*format = format_ptr;
+	*format += 1;
+	print_arg(**format, ap, byte);
+	*format += 1;
 }
