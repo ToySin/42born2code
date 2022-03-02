ac4e9f5c3282777c02a38b87e0e1bd6012991c2b
Dongbin <toysin1203@hotmail.com>
2022-03-03 00:39:03 +0900

    [U] etc

---
+++ so_long/include/so_long.h
@@ -0,0 +1,13 @@
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <fcntl.h>
+
+# include "error.h"
+# include "../lib/include/libft.h"
+# include "../lib/include/get_next_line_bonus.h"
+
+
+
+
+#endif
