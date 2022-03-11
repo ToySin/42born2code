d3df3bd172b9589f188f6849d733ca0ccbb3977c
Toysin <toysin1203@hotmail.com>
2022-03-11 13:16:21 +0900

    [A] key.h

---
+++ so_long/include/key.h
@@ -0,0 +1,22 @@
+#ifndef KEY_H
+# define KEY_H
+
+# define X_EVENT_KEY_PRESS 2
+# define X_EVENT_KEY_EXIT 17
+
+# define KEY_R 15
+# define KEY_ESC 63
+
+# define KEY_A 0
+# define KEY_S 1
+# define KEY_D 2
+# define KEY_W 13
+
+# define KEY_LEFT 123
+# define KEY_RIGHT 124
+# define KEY_DOWN 125
+# define KEY_UP 126
+
+
+
+#endif
